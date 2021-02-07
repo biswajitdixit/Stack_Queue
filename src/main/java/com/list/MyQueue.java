e4e316bccbf8a168f554b383441718bc4a1bbd56
biswajitdixit <buntyoec1998@gmail.com>
2021-02-07 11:37:34 +0530

    ADDED: create a queue

---
+++ src/main/java/com/list/MyQueue.java
@@ -0,0 +1,30 @@
+package com.list;
+
+public class MyQueue {
+    public final MyLinkedList myLinkedList;
+
+    public MyQueue() {
+        myLinkedList = new MyLinkedList();
+    }
+
+    public void enqueue(MyNode element) {
+        myLinkedList.append(element);
+    }
+
+    public void printStack() {
+        myLinkedList.printMyNodes();
+    }
+    public boolean isEmpty() {
+        return myLinkedList.head == null;
+    }
+
+    public INode peak() {
+        if (!isEmpty()) {
+            return myLinkedList.head;
+        } else {
+            System.out.println("Queue is empty");
+
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/list/MyQueueTest.java
@@ -0,0 +1,20 @@
+package com.list;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+   @Test
+    public void given3Numbers_WhenAddedToQueue_ShouldPassResult(){
+       MyNode<Integer> myFirstNode = new MyNode<>(56);
+       MyNode<Integer> mySecondNode = new MyNode<>(30);
+       MyNode<Integer> myThirdNode = new MyNode<>(70);
+       MyQueue myQueue = new MyQueue();
+       myQueue.enqueue(myFirstNode);
+       myQueue.enqueue(mySecondNode);
+       myQueue.enqueue(myThirdNode);
+       myQueue.printStack();
+       INode peak = myQueue.peak();
+       Assert.assertEquals(myFirstNode, peak);
+   }
+}
